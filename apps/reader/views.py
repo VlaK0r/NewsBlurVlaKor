71014d00cf69df2ce8e1861e749b812d0d2da844
VlaK0r <77666575+VlaK0r@users.noreply.github.com>
2026-02-13 19:15:33 +0300

    Update views.py 40

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -888,7 +888,7 @@ def load_single_feed(request, feed_id):
     user = get_user(request)
     # offset                  = int(request.GET.get('offset', 0))
     # limit                   = int(request.GET.get('limit', 40))
-    limit = 6
+    limit = 40
     page = int(request.GET.get("page", 1))
     delay = int(request.GET.get("delay", 0))
     offset = limit * (page - 1)
